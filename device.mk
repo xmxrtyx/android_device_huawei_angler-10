da1350621046ce361e80c21f29a44398d0e21893
Anay Wadhera <anay1018@gmail.com>
2019-11-21 19:06:31 -0800

    angler-common: build additional GNSS packages
    Change-Id: I29ad57dc609d165fe4dcfac4aec787824beb8996

--- device.mk
+++ device.mk
@@ -251,6 +251,8 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
     libgps.utils \
+    libgnss \
+    liblocation_api \
     gps.msm8994
 
 PRODUCT_PACKAGES += \
@@ -342,7 +344,8 @@ PRODUCT_PACKAGES += \
 
 #GNSS HAL
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl
+    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service
 
 # Health HAL
 PRODUCT_PACKAGES += \
